3ee350751b4307db4d1cde4cc6cd8549055afc11
Sukhchandan <sukhchandan.95@gmail.com>
2020-01-25 19:23:57 -0400

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members  = [
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+
+    },{
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+
+    },{
+        id: 3,
+        name: 'shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
